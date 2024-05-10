ae7622377d503b793469d4b8be1223e765123396
boxfish <fuguo892@gmail.com>
2024-05-10 15:39:37 +0900

    fix: updateDronePosとsetDronePosが全く同じ関数になっていた問題を修正

--- q1/lib/dronePos.py
+++ q1/lib/dronePos.py
@@ -7,8 +7,8 @@ class DronePos:
         return (self._x, self._y)
 
     def updateDronePos(self, x: float, y: float):
-        self._x = x
-        self._y = y
+        self._x = self._x + x
+        self._y = self._y + y
 
     def setDronePos(self, x: float, y: float):
         self._x = x
