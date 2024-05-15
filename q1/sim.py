52cf16904f0ec6804d5c80966301c3aa81ce1118
boxfish <boxfish893@gmail.com>
2024-05-15 15:00:19 +0900

    refactor: simをクラス化

--- q1/sim.py
+++ q1/sim.py
@@ -1,59 +1,55 @@
 import numpy as np
 from drone import Drone
 
-A = 10000
-B = 10000
-V = 10
-rang = 300
 
-contacted = False
-topCount = 0
-buttomCount = 0
+class Sim:
+    _contacted = False
+    _topCount = 0
+    _buttomCount = 0
 
+    def __init__(self, a, b, v, rng):
+        self._n1 = Drone(a, b, v)
+        self._n2 = Drone(a, b, v)
+        self._rang = rng
 
-def check_distace(distance):
-    return rang > distance
+    def check_distace(self, distance):
+        return self._rang > distance
 
+    def counter(self, contact: bool):
+        if contact:
+            self._contacted = True
+        else:
+            if self._contacted:
+                self._buttomCount += 1
+            self._topCount += 1
+            self._contacted = False
 
-def counter(contact: bool):
-    global contacted
-    global topCount
-    global buttomCount
-    if contact:
-        contacted = True
-    else:
-        if contacted:
-            buttomCount += 1
-        topCount += 1
-        contacted = False
+    def main(self):
 
+        T = 1000000
+        res = 1
 
-def main():
-    n1 = Drone(A, B, V)
-    n2 = Drone(A, B, V)
+        n1route = []
+        n2route = []
+        distances = []
+        contacts = []
 
-    n1route = []
-    n2route = []
-    distances = []
-    contacts = []
+        for i in range(1, T):
+            t = i / res
+            self._n1.advanceTime(t)
+            self._n2.advanceTime(t)
+            n1route.append(self._n1.drone)
+            n2route.append(self._n2.drone)
+            if (i / T) * 100 % 10 == 0:
+                print("rang:", self._rang, "progress:", (i / T) * 100, "%")
 
-    for i in range(1, 1000000):
-        t = i
-        n1.advanceTime(t)
-        n2.advanceTime(t)
-        n1route.append(n1.drone)
-        n2route.append(n2.drone)
+        distances = np.linalg.norm(np.array(n1route) - np.array(n2route), axis=1)
+        contacts = distances < self._rang
 
-    distances = np.linalg.norm(np.array(n1route) - np.array(n2route), axis=1)
-    contacts = distances < rang
+        for contact in contacts:
+            self.counter(contact)
 
-    for contact in contacts:
-        counter(contact)
-
-    print(topCount / buttomCount)
-    print(topCount)
-    print(buttomCount)
-
-
-if __name__ == "__main__":
-    main()
+        print(self._topCount / self._buttomCount)
+        print(self._topCount)
+        print(self._buttomCount)
+        return self._topCount / self._buttomCount
